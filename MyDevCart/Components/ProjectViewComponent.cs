2699bcdadf9ff07f6f2f746ba4407e32f877e182
AlirezaNoor <alirezang7575@gmail.com>
2022-03-10 12:45:27 +0330

    We Use ViewComponent in our project

---
+++ MyDevCart/Components/ProjectViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyDevCart.Models;
+
+namespace MyDevCart.Components
+{
+    public class ProjectViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>()
+            {
+                new Project( 1 ,"محصول شماره یک","این یک محصول است که یه شما کمک می کند","project-1.jpg","nasa"),
+                new Project( 2 ,"محصول شماره دو","این یک محصول است که یه شما کمک می کند","project-2.jpg","nasa"),
+                new Project( 3 ,"محصول شماره سه","این یک محصول است که یه شما کمک می کند","project-3.jpg","nasa"),
+                new Project( 4 ,"محصول شماره چهارم","این یک محصول است که یه شما کمک می کند","project-4.jpg","nasa"),
+
+
+            };
+            return View("_project",projects);
+        }
+    }
+}
---
+++ MyDevCart/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿namespace MyDevCart.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string discriptionn { get; set; }
+        public string Image { get; set; }
+        public string client { get; set; }
+
+        public Project(long id, string name, string discriptionn, string image, string client)
+        {
+            Id = id;
+            Name = name;
+            this.discriptionn = discriptionn;
+            Image = image;
+            this.client = client;
+        }
+    }
+}
