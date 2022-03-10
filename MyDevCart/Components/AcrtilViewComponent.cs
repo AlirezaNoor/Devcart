73ee608d2bb960896729f1cdc762c224034ce8d0
AlirezaNoor <alirezang7575@gmail.com>
2022-03-10 20:05:32 +0330

    Partial&ViewCompont

---
+++ MyDevCart/Components/AcrtilViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyDevCart.Models;
+
+namespace MyDevCart.Components
+{
+    public class AcrtilViewComponent:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var acrtils = new List<Acrtical>()
+            {
+                new Acrtical(1, "angular", "this maghale is 4 u", "blog-post-thumb-1.jpg"),
+                new Acrtical(2, "react", "this maghale is 4 u", "blog-post-thumb-2.jpg"),
+                new Acrtical(3, "agular2", "this maghale is 4 u", "blog-post-thumb-3.jpg"),
+                new Acrtical(4, "view", "this maghale is 4 u", "blog-post-thumb-4.jpg"),
+
+            };
+            return View("Acrtil",acrtils);
+        }
+    }
+}
---
+++ MyDevCart/Models/Acrtical.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MyDevCart.Models
+{
+    public class Acrtical
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public string disciption { get; set; }
+        public string Image { get; set; }
+
+        public Acrtical(long id, string name, string disciption, string image)
+        {
+            Id = id;
+            this.name = name;
+            this.disciption = disciption;
+            Image = image;
+        }
+    }
+}
